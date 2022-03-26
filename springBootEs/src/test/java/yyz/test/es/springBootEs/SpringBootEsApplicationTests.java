57ede453b3cd311743c8fd46aea2d44bf5101231
yuanyongzhuang <yuanyongzhuang>
2022-03-26 17:47:09 +0800

    git过滤

---
+++ springBootEs/src/test/java/yyz/test/es/springBootEs/SpringBootEsApplicationTests.java
@@ -0,0 +1,13 @@
+package yyz.test.es.springBootEs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
